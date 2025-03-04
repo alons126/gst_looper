fb27e449de984d630c9b68393cafdac4df976347
alons126 <alonsportes@mail.tau.ac.il>
2025-03-04 15:05:20 +0200

    test 3

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 echo ""
-git pull
+# git pull
 echo ""
 echo "\033[35m- Re-pulling repository -----------------------------------------------\033[0m"
 echo ""
-git reset --hard
-git pull
+# git reset --hard
+# git pull
 echo ""
 # echo "\033[35m- Cleaning excessive file ---------------------------------------------\033[0m"
 # echo ""
